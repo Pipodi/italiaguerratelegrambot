5fde14e622ac1f1386def0124a6944208a0d7613
Alex Collini <pipodi93@gmail.com>
2019-06-10 20:36:53 +0200

    Fix SQLite queries

--- src/main/java/it/pipodi/italiaguerratelegrambot/bot/ItaliaGuerraBotListener.java
+++ src/main/java/it/pipodi/italiaguerratelegrambot/bot/ItaliaGuerraBotListener.java
@@ -7,58 +7,70 @@ import twitter4j.Status;
 import twitter4j.StatusDeletionNotice;
 import twitter4j.StatusListener;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class ItaliaGuerraBotListener implements StatusListener {
 
-    private ItaliaGuerraTelegramBot bot;
-    private long chatId;
+	private ItaliaGuerraTelegramBot bot;
+	private Connection db;
 
-    public ItaliaGuerraBotListener(ItaliaGuerraTelegramBot bot, long chatId){
-        this.bot = bot;
-        this.chatId = chatId;
-    }
+	public ItaliaGuerraBotListener(ItaliaGuerraTelegramBot bot, Connection db) {
+		this.bot = bot;
+		this.db = db;
+	}
 
-    @Override
-    public void onStatus(Status status) {
-        if (!status.isRetweet() && status.getUser().getScreenName().equals("italiaguerrabot")) {
-            System.out.println("[LOG] Status received: " + status.toString());
-            SendMessage message = new SendMessage().setChatId(this.chatId).setText(status.getText());
-            try {
-                this.bot.execute(message);
-            } catch (TelegramApiException e) {
-                e.printStackTrace();
-            }
-        }else {
-            System.out.println("Invalid status received: " + status.getText());
-        }
+	@Override
+	public void onStatus(Status status) {
+		if (!status.isRetweet() && status.getUser().getScreenName().equals("italiaguerrabot")) {
+			System.out.println("[LOG] Status received: " + status.toString());
 
-    }
+			String tweetText = status.getText();
 
-    @Override
-    public void onDeletionNotice(StatusDeletionNotice statusDeletionNotice) {
+			Statement select = null;
+			try {
+				select = db.createStatement();
+				ResultSet resultSet = select.executeQuery("select * from chatids");
+				while (resultSet.next()) {
+					String chatId = resultSet.getString("chatid");
+					SendMessage message = new SendMessage().setChatId(Long.valueOf(chatId)).setText(tweetText);
+					bot.execute(message);
+				}
+			} catch (SQLException | TelegramApiException e) {
+				e.printStackTrace();
+			}
 
-    }
 
-    @Override
-    public void onTrackLimitationNotice(int i) {
+		} else {
+			System.out.println("Invalid status received: " + status.getText());
+		}
 
-    }
+	}
 
-    @Override
-    public void onScrubGeo(long l, long l1) {
+	@Override
+	public void onDeletionNotice(StatusDeletionNotice statusDeletionNotice) {
 
-    }
+	}
 
-    @Override
-    public void onStallWarning(StallWarning stallWarning) {
+	@Override
+	public void onTrackLimitationNotice(int i) {
 
-    }
+	}
 
-    @Override
-    public void onException(Exception e) {
+	@Override
+	public void onScrubGeo(long l, long l1) {
 
-    }
+	}
 
-    public void setChatId(long chatId){
-        this.chatId = chatId;
-    }
+	@Override
+	public void onStallWarning(StallWarning stallWarning) {
+
+	}
+
+	@Override
+	public void onException(Exception e) {
+
+	}
 }
--- src/main/java/it/pipodi/italiaguerratelegrambot/bot/ItaliaGuerraTelegramBot.java
+++ src/main/java/it/pipodi/italiaguerratelegrambot/bot/ItaliaGuerraTelegramBot.java
@@ -4,90 +4,85 @@ import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-import twitter4j.*;
+import twitter4j.FilterQuery;
+import twitter4j.TwitterStream;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class ItaliaGuerraTelegramBot extends TelegramLongPollingBot {
 
-    private String telegramAPIKey;
-    private TwitterStream twitterStream;
-    private long userId;
-    private Connection db;
+	private static ItaliaGuerraTelegramBot instance = null;
+	private String telegramAPIKey;
+	private Connection db;
 
-    private static ItaliaGuerraTelegramBot instance = null;
+	private ItaliaGuerraTelegramBot(TwitterStream twitterStream, String telegramAPIKey, long userId, Connection db) {
+		this.telegramAPIKey = telegramAPIKey;
+		this.db = db;
+		ItaliaGuerraBotListener listener = new ItaliaGuerraBotListener(this, db);
+		twitterStream.addListener(listener);
+		FilterQuery filterQuery = new FilterQuery(userId);
+		twitterStream.filter(filterQuery);
+	}
 
-    public static ItaliaGuerraTelegramBot getInstance(TwitterStream twitterStream, String telegramAPIKey, long userId, Connection db){
-        if (instance != null){
-            return instance;
-        } else {
-          instance = new ItaliaGuerraTelegramBot(twitterStream, telegramAPIKey, userId, db);
-          return instance;
-        }
+	public static ItaliaGuerraTelegramBot getInstance(TwitterStream twitterStream, String telegramAPIKey, long userId, Connection db) {
+		if (instance != null) {
+			return instance;
+		} else {
+			instance = new ItaliaGuerraTelegramBot(twitterStream, telegramAPIKey, userId, db);
+			return instance;
+		}
 
-    }
+	}
 
-    private ItaliaGuerraTelegramBot(TwitterStream twitterStream, String telegramAPIKey, long userId, Connection db){
-        this.telegramAPIKey = telegramAPIKey;
-        this.twitterStream = twitterStream;
-        this.userId = userId;
-        this.db = db;
-    }
+	@Override
+	public void onUpdateReceived(Update update) {
+		if (update.getMessage().getText().equals("/start")) {
+			Statement select = null;
+			try {
+				select = db.createStatement();
+				ResultSet resultSet = select.executeQuery("select * from chatids");
+				while (resultSet.next()) {
+					String chatId = resultSet.getString("chatid");
+					System.out.println("[LOG] chatId in db: " + chatId);
+					if (chatId.equals(String.valueOf(update.getMessage().getChatId()))) {
+						SendMessage message = new SendMessage().setChatId(update.getMessage().getChatId()).setText("Bot già avviato.")
+								.setParseMode("Markdown");
+						try {
+							execute(message);
+						} catch (TelegramApiException e) {
+							e.printStackTrace();
 
+						}
+						return;
+					}
+				}
+				String query = "insert into chatids (chatid) values (?)";
+				PreparedStatement statement = db.prepareStatement(query);
+				statement.setString(1, String.valueOf(update.getMessage().getChatId()));
+				statement.executeUpdate();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			SendMessage message = new SendMessage().setChatId(update.getMessage().getChatId())
+					.setText("Bot avviato. In attesa di aggiornamenti da parte di [ItaliaGuerraBot 2020](https://twitter.com/italiaguerrabot)")
+					.setParseMode("Markdown");
+			try {
+				execute(message);
+			} catch (TelegramApiException e) {
+				e.printStackTrace();
+			}
+		}
 
-    @Override
-    public void onUpdateReceived(Update update) {
-        if (update.getMessage().getText().equals("/start")) {
-            Statement select = null;
-            try {
-                select = db.createStatement();
-                ResultSet resultSet = select.executeQuery("select * from chatids");
-                while (resultSet.next()){
-                    String chatId = resultSet.getString("chatid");
-                    System.out.println("[LOG] chatId in db: " + chatId);
-                    if (chatId.equals(String.valueOf(update.getMessage().getChatId()))){
-                        SendMessage message = new SendMessage().setChatId(update.getMessage().getChatId()).setText("Bot già avviato.").setParseMode("Markdown");
-                        try {
-                            execute(message);
-                        } catch (TelegramApiException e) {
-                            e.printStackTrace();
-                        }
-                        return;
-                    }
-                }
-                Statement insert = db.createStatement();
-                String  query = String.format("insert into chatids (chatid) values ('%s')", String.valueOf(update.getMessage().getChatId()));
-                insert.execute(query);
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
+	}
 
+	@Override
+	public String getBotUsername() {
+		return "italiaguerratelegrambot";
+	}
 
-            ItaliaGuerraBotListener listener = new ItaliaGuerraBotListener(this, update.getMessage().getChatId());
-            this.twitterStream.addListener(listener);
-            FilterQuery filterQuery = new FilterQuery(userId);
-            twitterStream.filter(filterQuery);
-            SendMessage message = new SendMessage().setChatId(update.getMessage().getChatId()).setText("Bot avviato. In attesa di aggiornamenti da parte di [ItaliaGuerraBot 2020](https://twitter.com/italiaguerrabot)").setParseMode("Markdown");
-            try {
-                execute(message);
-            } catch (TelegramApiException e) {
-                e.printStackTrace();
-            }
-        }
-
-    }
-
-    @Override
-    public String getBotUsername() {
-        return "italiaguerratelegrambot";
-    }
-
-    @Override
-    public String getBotToken() {
-        return telegramAPIKey;
-    }
+	@Override
+	public String getBotToken() {
+		return telegramAPIKey;
+	}
 
 }
